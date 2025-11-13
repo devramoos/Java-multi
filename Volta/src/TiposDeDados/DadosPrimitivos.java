5620b391a6347bc9c11ef589a852d80b5761dc27
Rodrigo Ramos <rodrigojgramos@outlook.com>
2025-11-13 06:55:01 -0300

    Version one

---
+++ Volta/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,12 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    static void main(String[]arguments) {
+        string naruto = "Naruto";
+        int idade = 16;
+        char inicial = 'N';
+        boolean vivioumorto = true;
+        long saldoBancario = 14567;
+
+    }
+}
